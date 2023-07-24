761b775a08ddd47753d2780538d985baa0b2c56d
Jeff Ock <jeffock1656@gmail.com>
2023-07-24 19:24:21 +0000

    changed font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = { "AgaveNerdFontMono-Regular:size=12"};
+static const char dmenufont[]       = "AgaveNerdFontMono-Regular:size=11";
 // background
 static const char col_gray1[]       = "#222222";
 // inactive window
